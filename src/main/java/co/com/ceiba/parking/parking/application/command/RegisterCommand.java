5b3512d9f8b62fdd32109608397ab5c36a39bbae
evergara <ee-web@hotmail.com>
2019-07-02 16:05:27 -0500

    Mejoras sonar

--- src/main/java/co/com/ceiba/parking/parking/application/command/RegisterCommand.java
+++ src/main/java/co/com/ceiba/parking/parking/application/command/RegisterCommand.java
@@ -4,11 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.util.Date;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-public class RegisterCommand {
+public class RegisterCommand implements Serializable {
+        private static final long serialVersionUID = 42L;
+
         private Integer id;
         private String vehicleType;
         private String licensePlate;
