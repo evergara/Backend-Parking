4f48f3f5b51956438a323662b31fb4e5d142ccf4
evergara <ee-web@hotmail.com>
2019-07-04 17:36:34 -0500

    Refactory nombre de metodos, clases y unificacion de idioma

--- src/main/java/co/com/ceiba/parking/parking/infrastructure/adapter/AdapterParkingRepository.java
+++ src/main/java/co/com/ceiba/parking/parking/infrastructure/adapter/AdapterParkingRepository.java
@@ -1,21 +1,21 @@
 package co.com.ceiba.parking.parking.infrastructure.adapter;
 
 import co.com.ceiba.parking.parking.domain.model.Registry;
-import co.com.ceiba.parking.parking.domain.repository.IPortRegistryRepository;
+import co.com.ceiba.parking.parking.domain.repository.PortRegistryRepository;
 import co.com.ceiba.parking.parking.infrastructure.entity.RegistryEntity;
-import co.com.ceiba.parking.parking.infrastructure.repository.IRepositoryParkingJPA;
+import co.com.ceiba.parking.parking.infrastructure.repository.RepositoryParkingJPA;
 import co.com.ceiba.parking.parking.infrastructure.mapper.RegistryMapper;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public class AdapterParkingRepository implements IPortRegistryRepository {
+public class AdapterParkingRepository implements PortRegistryRepository {
 
-    private IRepositoryParkingJPA repositoryParkingJPA;
+    private RepositoryParkingJPA repositoryParkingJPA;
     private RegistryMapper registryMapper;
 
-    public AdapterParkingRepository(IRepositoryParkingJPA repositoryParkingJPA, RegistryMapper registryMapper){
+    public AdapterParkingRepository(RepositoryParkingJPA repositoryParkingJPA, RegistryMapper registryMapper){
         this.repositoryParkingJPA = repositoryParkingJPA;
         this.registryMapper = registryMapper;
     }
