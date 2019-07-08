2a4ee7d81856d84f16b0bb9cd9409a760b13d059
evergara <ee-web@hotmail.com>
2019-07-08 16:18:12 -0500

    Refactory nombre de metodos, clases y unificacion de idioma

--- src/main/java/co/com/ceiba/parking/parking/infrastructure/controller/ParkingController.java
+++ src/main/java/co/com/ceiba/parking/parking/infrastructure/controller/ParkingController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/Parking")
+@RequestMapping("/parking")
 public class ParkingController {
 
     private RegisterEntryHandler registerHandler;
